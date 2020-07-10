72b89510d1e24e637c6b08ce4287b4a49c9f7d35
gordon <gordon>
2020-07-10 13:41:34 +0800

    Add design file

--- MPApp/e2e/protractor.conf.js
+++ MPApp/e2e/protractor.conf.js
@@ -1,28 +1,3 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
-
-const { SpecReporter } = require('jasmine-spec-reporter');
-
-exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './src/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.json')
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:12b2455355d8b8b0d09d9e06e82eca824190009a3f0743841da8bacad462613d
+size 749
